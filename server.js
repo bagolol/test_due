893cd2eeb13ee066fb1d23baf063b1ebf81fddc6
Rocco <roccorighi@hotmail.com>
2015-11-06 16:16:38 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res, err) {
+    if (err) throw err;
+    res.send('index.html');
+});
+
+
+app.get('/issues', function (req, res) {
+  fs.readFile('./issues.json','utf8', function (err, data) {
+    if (err) throw err;
+    var prices = JSON.parse(data);
+    res.send(prices);
+  });
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('server listening at http://%s:%s', host, port);
+});
+
