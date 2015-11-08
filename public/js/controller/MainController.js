cde8f0699037daded41d9144ffc05036afbcf85e
Rocco <roccorighi@hotmail.com>
2015-11-08 17:57:25 +0000

    adds error callback in mainCtrl

--- public/js/controller/MainController.js
+++ public/js/controller/MainController.js
@@ -1,9 +1,14 @@
-app.controller('MainCtrl', function($scope, issueFinder) {
+app.controller('MainCtrl', ['$scope', 'issueFinder', function($scope, issueFinder) {
   var myData = issueFinder.serverCall();
-  myData.then(function (result) {
-    $scope.issues = result;
-  });
-});
+  myData.then(
+    function (result) {
+      $scope.issues = result;
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+}]);
 
   // $scope.go = function (issue) {
   //   $scope.filters = issue;
